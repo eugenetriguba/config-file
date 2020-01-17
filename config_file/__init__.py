c5bc60adfc90b0e239fb335da44c5a8d4cbe365d
Eugene Triguba <eugenetriguba@gmail.com>
2020-01-17 15:57:12 -0600

    Bump version: 0.2.0 → 0.3.0

--- config_file/__init__.py
+++ config_file/__init__.py
@@ -1,7 +1,7 @@
 from config_file.config_file import ConfigFile
 from config_file.parsers.base_parser import BaseParser, ParsingError
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __all__ = [
     "ConfigFile",
