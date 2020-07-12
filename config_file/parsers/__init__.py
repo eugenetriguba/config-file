b467d83e7d67dbda82663634ff60e975f722b115
Eugene Triguba <eugenetriguba@gmail.com>
2020-07-12 09:11:27 -0500

    Add to __all__

--- config_file/parsers/__init__.py
+++ config_file/parsers/__init__.py
@@ -1,6 +1,23 @@
 from .base_parser import BaseParser
 from .ini_parser import IniParser
 from .json_parser import JsonParser
-from .parse_value import parse_value
+from .toml_parser import TomlParser
+from .yaml_parser import YamlParser
+from .parse_value import (
+    parse_value,
+    can_be_parsed_as_bool,
+    can_be_parsed_as_float,
+    can_be_parsed_as_int
+)
 
-__all__ = ["BaseParser", "IniParser", "JsonParser", "parse_value"]
+__all__ = [
+    "BaseParser",
+    "IniParser",
+    "JsonParser",
+    "TomlParser",
+    "YamlParser",
+    "parse_value",
+    "can_be_parsed_as_bool",
+    "can_be_parsed_as_float",
+    "can_be_parsed_as_int"
+]
