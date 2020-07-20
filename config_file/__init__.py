6eab76b24dfc831b8c41c445d98175c3276b20a5
Eugene Triguba <eugenetriguba@gmail.com>
2020-07-19 20:28:40 -0500

    Add UnrecognizedFileError and MissingDependencyError

--- config_file/__init__.py
+++ config_file/__init__.py
@@ -1,12 +1,19 @@
 from config_file.config_file import ConfigFile
-from config_file.exceptions import ConfigFileError, ParsingError
+from config_file.exceptions import (
+    ConfigFileError,
+    MissingDependencyError,
+    ParsingError,
+    UnrecognizedFileError,
+)
 from config_file.parsers import AbstractParser
 
 __version__ = "0.10.0"
 
 __all__ = [
     "ConfigFile",
-    "ParsingError",
-    "ConfigFileError",
     "AbstractParser",
+    "ConfigFileError",
+    "ParsingError",
+    "UnrecognizedFileError",
+    "MissingDependencyError",
 ]
--- config_file/exceptions.py
+++ config_file/exceptions.py
@@ -4,3 +4,16 @@ class ConfigFileError(Exception):
 
 class ParsingError(ConfigFileError):
     """Unable to parse the configuration file."""
+
+
+class MissingDependencyError(ConfigFileError):
+    """
+    PyYaml or toml is not installed, but the parsers
+    for them were attempted to be used.
+    """
+
+
+class UnrecognizedFileError(ConfigFileError):
+    """
+    A missing file extension or a file type not supported.
+    """
