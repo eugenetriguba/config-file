fb34cb2b000e425c152bc7d4de5c5227bed138e4
Eugene Triguba <eugenetriguba@gmail.com>
2020-08-05 21:15:47 -0500

    Remove AbstractParser from __init__

--- config_file/__init__.py
+++ config_file/__init__.py
@@ -1,9 +1,5 @@
-from .abstract_parser import AbstractParser
 from .config_file import ConfigFile
 
 __version__ = "0.10.0"
 
-__all__ = [
-    "ConfigFile",
-    "AbstractParser",
-]
+__all__ = ["ConfigFile"]
