d2f3161dac2313e4109ba2159fdc0d43e50506f8
Eugene Triguba <eugenetriguba@gmail.com>
2020-08-06 09:43:44 -0500

    Test extras parsers & move imports inside those parsers

--- config_file/toml_parser.py
+++ config_file/toml_parser.py
@@ -1,27 +1,18 @@
-try:
-    import tomlkit
-
-    TOML_AVAILABLE = True
-except ImportError:
-    TOML_AVAILABLE = False
-
-
 from .base_parser import BaseParser
 
 
 class TomlParser(BaseParser):
     def __init__(self, file_contents: str):
-        if not TOML_AVAILABLE:
+        try:
+            import tomlkit
+            from tomlkit.exceptions import ParseError
+
+            super().__init__(
+                file_contents=file_contents, module=tomlkit, decode_error=ParseError,
+            )
+        except ImportError:
             raise ImportError(
                 "It doesn't appear `tomlkit` is installed, but a toml "
                 "file was attempted to be used. Install the `toml` "
                 "extra first with `pip install config-file[toml]`."
             )
-
-        super().__init__(
-            file_contents=file_contents,
-            module=tomlkit,
-            # tomlkit doesn't expose it's exceptions, but
-            # it's parsing error is a subclass of ValueError
-            decode_error=ValueError,
-        )
